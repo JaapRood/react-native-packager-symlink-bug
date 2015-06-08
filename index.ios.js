203f7a67f132092c3647ca1090f185a77a43a48b
Jaap Rood <jaap@jaaprood.nl>
2015-06-08 16:25:35 +1000

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+
+var PackageResolveExample = require('app/components/app');
+
+AppRegistry.registerComponent('PackageResolveExample', () => PackageResolveExample);
